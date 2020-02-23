07326afdb0b68f6791d3f8bd7a34272e680303fc
leyuri <leyuri97@gmail.com>
2020-02-23 23:48:20 +0900

    Get the Location

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import Loading from "./Loading";
+import {Alert} from "react-native";
+import * as Location from "expo-location";
 
-export default function App() {
-  return <Loading/>;
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+
+  getLoaction = async () => { 
+    try {
+      // throw Error();
+      await Location.requestPermissionsAsync();
+      const { coords: {latitude, longitude} } = await Location.getCurrentPositionAsync();
+      // Send to API and get weather!
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad");
+    }
+  };
+  componentDidMount() {
+    this.getLoaction();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading/> : null;
+  }
 }
 
