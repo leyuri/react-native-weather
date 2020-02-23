f02f58dd92a8459dd4c08b4a5402a333c886b4b8
leyuri <leyuri97@gmail.com>
2020-02-23 22:52:33 +0900

    Layout with Flexbox

--- App.js
+++ App.js
@@ -4,16 +4,22 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.yellowView}/>
+      <View style={styles.blueView}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  yellowView: {
+    flex: 2,
+    backgroundColor: "yellow"
+  },
+  blueView: {
+    flex: 2,
+    backgroundColor: "blue"
+  }
 });
