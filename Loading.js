d6aecea885f51958942b4bfd076e61fe95abb9fb
leyuri <leyuri97@gmail.com>
2020-02-23 23:11:52 +0900

    Add Loading Screen

---
+++ Loading.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Loading() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Getting the fucking weather</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-end",
+        paddingHorizontal: 30,
+        paddingVertical: 100,
+        backgroundColor: "#d5f4e6"
+    },
+    text: {
+        color: "#3e4444",
+        fontSize: 30
+
+    }
+});
+
+ 
