a588185301338fbd298abf631d8ea00d733e6273
leyuri <leyuri97@gmail.com>
2020-02-24 16:04:30 +0900

    Add Icons

--- Weather.js
+++ Weather.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function Weather({ temp }) {
     return (
         <View style={styles.container}>
-            <Text>{temp}</Text>
+            <View style={styles.halfcontainer}>
+                <MaterialCommunityIcons 
+                size={90} 
+                name="weather-lightning-rainy"/>
+                <Text style={styles.temp}>{temp}°</Text>
+            </View>
+            <View style={styles.halfcontainer}/>
         </View>
     );
 }
@@ -28,6 +35,14 @@ Weather.propTypes = {
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    temp: {
+    fontSize: 32
+    },
+    halfcontainer: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
